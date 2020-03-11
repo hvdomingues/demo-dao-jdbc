136ff57ceb862e4ff107ab6e9dfe33609187ee5e
Henrique V. Domingues <hvdomingues1@gmail.com>
2020-03-10 22:29:07 -0300

    Implementada melhoria de código para implementação do Seller e do Department

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -12,78 +12,88 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
-	
+
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public void insert(Seller obj) {
-		
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
-		
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
-		
-		try {
-			st = conn.prepareStatement(""
-					+ "SELECT seller.*,department.Name as DepName " + 
-					"FROM seller INNER JOIN department " + 
-					"ON seller.DepartmentId = department.Id " + 
-					"WHERE seller.Id = ?");
-			
-			st.setInt(1, id);
-			
-			rs = st.executeQuery();
-			if(rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				
-				Seller seller = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"),
-						rs.getDate("BirthDate"), rs.getDouble("BaseSalary"), dep);
-				
-				return seller;
-			}else {
-				return null;	
-			}
-			
 
-		}
-		catch (SQLException e) {
+		try {
+			st = conn.prepareStatement(
+					"" + "SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+
+			st.setInt(1, id);
+
+			rs = st.executeQuery();
+
+			if (rs.next()) {
+
+				return instantiateSeller(instantiateDepartment(rs), rs);
+
+			} else {
+				return null;
+			}
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		}		
-		
+		}
+
 	}
 
 	@Override
 	public List<Seller> findAll() {
-		
+
 		return null;
 	}
 
-	
-	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+
+		return dep;
+	}
+
+	private Seller instantiateSeller(Department dep, ResultSet rs) throws SQLException {
+
+		Seller sell = new Seller();
+
+		sell.setDepartment(dep);
+		sell.setId(rs.getInt("Id"));
+		sell.setName(rs.getString("Name"));
+		sell.setEmail(rs.getString("Email"));
+		sell.setBirthDate(rs.getDate("BirthDate"));
+		sell.setBaseSalary(rs.getDouble("BaseSalary"));
+
+		return sell;
+
+	}
+
 }
